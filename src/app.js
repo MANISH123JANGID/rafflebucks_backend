9dbea98651276ad4adc9bc66799aaae875a3ce44
MANISH JANGID <JANGID.K211@GMAIL.com>
2022-02-03 21:18:46 +0530

    FOR DEPLOYMENT ON HEROKU

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ const express= require('express');
 // DOTENV FOR ENVIRONMENT VARIABLES
 const env= require('dotenv');
 // IMPORTING PATH FOR STATIC PAGE 
- const path =require('path');
+ const path =require('path'); 
  // importing hbs for registering partials
  const hbs = require('hbs');
 // IMPORTING BODYPARSER FOR CONVERTING FROM JSON TO JS OBJECTS
@@ -14,35 +14,47 @@ const navRoutes= require('./routes/navroutes');
 //CREATING EXPRESS APP
 const app= express();
 //DATABASE CONNECTOR 
-require('./db/dbconnect')
+
+const port= process.env.PORT || 3000;
+require('./db/dbconnect');
 //CONFIGURING THE ENVIRONMENT VARIABLE
 env.config();
 // Getting the static path for the home page 
 const static_path= path.join(__dirname,"../public"); 
+
 // app using the static path 
-// app.use(express.static(static_path));
-// console.log(path.join(__dirname,"../public"));
+app.use(express.static(static_path));
+
 // bodyparser to convert the json into js object
-app.use(bodyParser.json());
-app.use(express.urlencoded({extetended:false}));
-//path fr the views folder 
+// app.use(bodyParser.json());
+
+app.use(bodyParser.urlencoded({extended:true}));
+//path fr the views folder  
 const template_path= path.join(__dirname,"../views");
+
 // path for the partials 
 const template_partials= path.join(__dirname,"../views/partials");
+
 // static path for the public folder to access the css, images and js 
-app.use(express.static("public"));
+// app.use(express.static("public"));
+
 // setting the view engine to hbs 
 app.set("view engine","hbs");
+
 //setting the path for views
 app.set("views",template_path);
+
 // for this we require hbs and then we register partials using the path
 hbs.registerPartials(template_partials);
+
 app.get('/',(req, res)=>{
     res.render("home_page");
 })
+
 // using the routes
 app.use('',userRoutes);
 app.use('',navRoutes);
+
 // listening on port 
 app.listen(process.env.PORT,()=>{
     console.log(process.env.PORT);
--- src/routes/navroutes.js
+++ src/routes/navroutes.js
@@ -1,12 +1,10 @@
 const express =require('express');
 const router = express.Router();
-
+ 
 router.get('/games', (req, res) => {
     res.render('game');
-});  
-
+})
 router.get('/mobiles',(req, res)=>{
     res.render('mobile_page');
 })
-
 module.exports = router;
